25260e5ac4b189e14d8083a3e1a2f54de913b738
marbin701 <marvin07012002@gmail.com>
2026-04-17 17:24:31 -0400

    Name generator

--- src/main/java/com/pluralsight/fullNameApplication.java
+++ src/main/java/com/pluralsight/fullNameApplication.java
@@ -1,9 +1,49 @@
 package com.pluralsight;
 
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        
+public class fullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First name: ");
+        String first = scanner.nextLine();
+
+
+        System.out.println("Middle name: ");
+        String middle = scanner.nextLine();
+
+
+        System.out.println("Last name: ");
+        String last = scanner.nextLine();
+
+        System.out.println("Suffix: ");
+        String suffix = scanner.nextLine();
+
+
+
+        first = first.trim();
+        middle = middle.trim();
+        last = last.trim();
+        suffix = suffix.trim();
+
+        String fullName = first;
+
+        if(!middle.isEmpty()){
+
+            fullName = fullName + " " + middle;
+        }
+
+        fullName = fullName + " " + last;
+
+        if(!suffix.isEmpty()){
+            fullName = fullName + ", " + suffix;
+
+        }
+
+        System.out.println("Full name: " + fullName);
+
+
     }
 }
--- src/main/java/com/pluralsight/fullNameParser.java
+++ src/main/java/com/pluralsight/fullNameParser.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class fullNameParser {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        input = input.trim();
+
+        String[] parts = input.split(" ");
+
+
+        if (parts.length == 2) {
+            String first = parts[0];
+            String middle = "(none)";
+            String last = parts[2];
+
+
+            System.out.println("First name: " + first);
+            System.out.println("Middle name: " + middle);
+            System.out.println("Last name : " + last);
+
+        } else if (parts.length == 3) {
+            String first = parts[0];
+            String middle = parts[1];
+            String last = parts[2];
+
+
+            System.out.println("First name: " + first);
+            System.out.println("Middle name: " + middle);
+            System.out.println("Last name : " + last);
+
+        }
+        else if (parts.length == 4) {
+            String first = parts[0];
+            String middle = parts[1];
+            String last = parts[2];
+
+
+            System.out.println("First name: " + first);
+            System.out.println("Middle name: " + middle);
+            System.out.println("Last name : " + last);
+
+        }
+    }
 }
